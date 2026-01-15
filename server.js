15d521712664833ee135fa161ce429e51140dfd3
CodeArtistDev <codeartistdev@gmail.com>
2026-01-16 00:27:46 +0530

    Complete backend

--- server.js
+++ server.js
@@ -32,7 +32,15 @@ app.use(
 app.use(express.json());
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+)
+
 app.use(xss());
 
 app.get('/', (req, res) => {
